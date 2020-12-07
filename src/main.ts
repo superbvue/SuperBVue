759e1b5ae0e71a26e4050b3f4d92c637f56c6f5b
Vu Tran <vutran6853@gmail.com>
2020-12-07 08:27:50 -0600

    FIX: export button and adjust config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
-import { createApp } from 'vue'
+import { createApp, Plugin } from 'vue'
 import App from './App.vue'
 import BButton from './components/buttons/BButton'
 
 import './assets/bootstrap.css'
 
-const app = createApp(App)
+// const app = createApp(App)
 
-app.component('BButton', BButton)
 
-app.mount('#app')
+// app.component('BButton', BButton)
+
+// app.mount('#app')
+// export default {
+//   install: (app: any, options: any) => {
+//     // Plugin code goes here
+//   }
+// }
+
+export {
+  BButton
+}
