19de05e29334c5be894720cee5c74072926e2d04
Vu Tran <vutran6853@gmail.com>
2020-12-24 04:26:47 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+import SBAccordion from '../src/components/accordion/SBAccordion'
+import SBAlert from '../src/components/alert/SBAlert'
+import SBBadge from '../src/components/badge/SBBadge'
+import SBBreadcrumb from '../src/components/breadcrumb/SBBreadcrumb'
+import SBButton from '../src/components/button/SBButton'
+import SBButtonGroup from '../src/components/buttonGroup/SBButtonGroup'
+import SBCard from '../src/components/card/SBCard'
+import SBCloseButton from '../src/components/closeButton/SBCloseButton'
+import SBNav from '../src/components/nav/SBNav'
+import SBProgress from '../src/components/progress/SBProgress'
+import SBProgressBar from '../src/components/progress/SBProgressBar'
+import SBFormSelect from '../src/components/form/SBFormSelect'
+import SBFormInput from '../src/components/form/SBFormInput'
+import SBContainer from '../src/components/layoutAndGridSystem/SBContainer'
+import SBRow from '../src/components/layoutAndGridSystem/SBRow'
+import SBCol from '../src/components/layoutAndGridSystem/SBCol'
+
+export {
+  SBAccordion,
+  SBAlert,
+  SBBadge,
+  SBBreadcrumb,
+  SBButton,
+  SBButtonGroup,
+  SBCard,
+  SBCloseButton,
+  SBNav,
+  SBProgress,
+  SBProgressBar,
+  SBFormSelect,
+  SBFormInput,
+  SBContainer,
+  SBRow,
+  SBCol
+}
